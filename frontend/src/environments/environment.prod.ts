a8bb5a77b04e55ffd461198482bfdfcdf20cc048
Matheus Peixoto <matheus-pxt@hotmail.com>
2022-10-04 23:53:26 -0300

    frontend has been deployed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  INVENTORY_URL: 'inventory-service/api',
-  ORDER_URL: 'order-service/api',
-  SHIPPING_URL: 'shipping-service/api',
+  INVENTORY_URL: 'http://inventory-service:8081/api',
+  ORDER_URL: 'http://order-service:8080/api',
+  SHIPPING_URL: 'http://shipping-service:8082/api',
 };
